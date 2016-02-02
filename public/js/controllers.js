c290455d44accfb62c017fc7e1c0af8c5207f381
konstantin <Rhfrfpz,hf1301>
2016-02-02 12:11:49 +0200

    last commit

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -8,24 +8,20 @@
 
 (function(){
     function MainCtrl ($scope, $http, URL){
-
         this.$scope = $scope;
         this.$http = $http;
         this.URL = URL;
         $scope.user = {};
-        this.getUser();
 
     }
 
-    MainCtrl.prototype.getUser = function(){
-        var $scope = this.$scope
+    MainCtrl.prototype.getUser = function($scope){
         this.$http.get(this.URL + 'user')
             .success(function(res){
-                debugger
             $scope.user = res
 
-        })
-    }
+            })
+    };
 
     angular.module('App')
         .controller('MainCtrl', MainCtrl)
